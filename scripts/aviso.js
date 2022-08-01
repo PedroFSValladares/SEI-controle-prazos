de630571eda9bf7bd9fca14d08f67b7db98c769b
Pedro Valladares <66287885+PedroFSValladares@users.noreply.github.com>
2022-08-01 12:29:17 -0300

    Add files via upload
    otimizado o funcionamento da extensão

--- scripts/aviso.js
+++ scripts/aviso.js
@@ -1,181 +1,181 @@
-/*
-//createPopUp();
-//setTimeout(() => {
-    //const TABLE_GERADOS = document.querySelectorAll("#tblProcessosGerados tbody td");
-    //const tableRecebidos = document.querySelectorAll("#tblProcessosRecebidos tbody td");
-    const PROCESSO_GERADOS = getProcessos("Gerados");
-    const PROCESSOS_RECEBIDOS = getProcessos("Recebidos");
-    const SERVIDOR_LOGADO = getServidor();
-    
-    verificaPrazos(PROCESSO_GERADOS, SERVIDOR_LOGADO);
-    verificaPrazos(PROCESSOS_RECEBIDOS, SERVIDOR_LOGADO);
+//window.addEventListener("loaded", init());
 
-    //}, 5000)
-    
-    function getProcessos(type){
-        let tableProcessosGerados = document.querySelectorAll(`#tblProcessos${type} tr`);
-        return Array.from(tableProcessosGerados).filter((processo, index) => {
-            return index >= 2;
-        });
-    }
-    function getServidor(){
-        const servidorLogadoInput = document.getElementById("hdnInfraPrefixoCookie");
-        return servidorLogadoInput.value.substring(servidorLogadoInput.value.lastIndexOf("_") + 1);
-    }
-    function verificaPrazos(processos, servidor){
-        let pr = [];
-        let numeroProcessos = "";
-        processos.forEach((processo) => {
-            let serv = processo.children[3].textContent.replace("(", "").replace(")", "");
-            if(processo.children[4].textContent !== "" && (serv === servidor || serv.textContent === "")){
-                pr.push(processo.children[2]);
-                numeroProcessos = numeroProcessos.concat(processo.children[2].textContent + "\n");
-            }
-            //console.log(processo);
-        });
-        if(pr.length != 0){
-            showPopup(pr);
-    }
-}
-function createPopUp(){
-    let txtAviso;
-    
-    const body = document.body;
-    const divTexto = document.createElement("div");
-    const backgroundDiv = document.createElement("div");
-    const centralDiv = document.createElement("div");
-    const closeButton = document.createElement("button");
-    
-    centralDiv.appendChild(closeButton);
-    
-    closeButton.addEventListener("click", (e) => {
-        backgroundDiv.style.visibility = "hidden";
-    })
-    
-    const pCSS = "z-index: 1;"
-    const backgroundDivStyle = backgroundDiv.style;
-    
-    backgroundDiv.classList.add("background-popup");
-    centralDiv.classList.add("popup");
-    divTexto.classList.add("popup-text");
-    
-    divTexto.textContent = "Os seguintes processos estão perto do prazo final:";
-    
-    centralDiv.appendChild(divTexto);
-    backgroundDiv.appendChild(centralDiv);
-    
-    body.append(backgroundDiv);
-}
-
-function showPopup(processos){
-    const backgroundDiv = document.querySelector(".popup");
-    const popupText = document.querySelector(".popup-text");
-    
-    console.log(processos);
-    processos.forEach((element) => {
-        var link = document.createElement("a");
-        link.href = element.children[0].href;
-        link.textContent = element.children[0].textContent;
-        link.style = element.children[0].style;
-        popupText.appendChild(link);
-    })
-    backgroundDiv.style.visibility = "visible";
-    let title = chrome.i18n.getMessage("notificationTitle");
-    //let content = chrome.i18n.getMessage("notificationContent", message.url);
-    
-}
-*/
-function teste(){
-    console.log("a")
+if(document.title === "SEI - Controle de Processos"){
+    setTimeout(() => {
+    }, 500)
 }
 setTimeout(() => {
-
     const TABLE_GERADOS = document.querySelectorAll("#tblProcessosGerados");
-    const PROCESSO_GERADOS = getProcessos("Gerados");
+    const PROCESSOS_GERADOS = getProcessos("Gerados");
     const PROCESSOS_RECEBIDOS = getProcessos("Recebidos");
     const SERVIDOR_LOGADO = getServidor();
-    
 
     creteTable(TABLE_GERADOS);
-
     
-    verificaPrazos(PROCESSO_GERADOS, SERVIDOR_LOGADO);
-    verificaPrazos(PROCESSOS_RECEBIDOS, SERVIDOR_LOGADO);
-    
-}, 2000)
-
+    //verificaPrazos(PROCESSOS_GERADOS, SERVIDOR_LOGADO);
+    verificaPrazos(SERVIDOR_LOGADO);
+    //verificaPrazos(PROCESSOS_RECEBIDOS, SERVIDOR_LOGADO); 
+},200)
 function getProcessos(type){
     let tableProcessos = document.querySelectorAll(`#tblProcessos${type} tr`);
     return Array.from(tableProcessos).filter((processo, index) => {
-        return index >= 2;
+        return index >= 1;
     });
 }
 function getServidor(){
     const servidorLogadoInput = document.getElementById("hdnInfraPrefixoCookie");
     return servidorLogadoInput.value.substring(servidorLogadoInput.value.lastIndexOf("_") + 1);
 }
-function verificaPrazos(processos, servidor){
-    let table = document.getElementById("tblProcessosComPrazos");
-    let pr = [];
-    processos.forEach((processo) => {
-        let serv = processo.children[3].textContent.replace("(", "").replace(")", "");
-        if(processo.children[4].textContent <= 2 && (serv === servidor || serv.textContent === "")){
+function verificaPrazos(/*processos,*/ servidor){
+    /*
+    let tableProcessos = Array.from(document.querySelectorAll("#divGeradosAreaTabela tr"));
+    console.log(tableProcessos);
+    let processos = tableProcessos.map((element) => {
+            return element.querySelectorAll("a");
+    });
+    let anchors = processos.filter((element, index) => {
+        if(index > 0)
+            return element;
+    })
+    console.log(anchors)
+    anchors.forEach((element, index) => {
+        anchors[index].forEach((element) => {
+            if(element.hasAttribute("onmouseover")){
+                console.log(element)
+            }
+        })
+    })
+    */
+   let processos = document.querySelectorAll("#divGeradosAreaTabela tr")
+   let anotacao = processos[1].querySelector("img[src='imagens/sei_anotacao_pequeno.gif']").parentNode
+   let table = document.getElementById("tblProcessosComPrazos");
+
+    if(getData(anotacao) > 0 && getData(anotacao) !== ""){
+        table.appendChild(anotacao.parentNode.parentNode);
+        sendMessage({"processos": anotacao.parentNode.parentNode});    
+    }
+   console.log(getData(anotacao));
+   /*
+   let table = document.getElementById("tblProcessosComPrazos");
+   let pr = [];
+   var prazo;
+   let data = document.createElement("td"); 
+   
+   data.align = "center";
+   console.log(processos)
+   
+   processos.forEach((processo) => {
+       let serv = processo.children[3].textContent.replace("(", "").replace(")", "");
+       
+       if(getData(processo) != ""){
+           data.textContent = getData(processo);
+           processo.appendChild(data)
+           table.appendChild(processo);
+           console.log(getData())
+           pr.push(processo);
+        }
+        if(processo.children[4].textContent != "" && (serv === servidor || serv.textContent === "")){
             table.appendChild(processo);
             pr.push(processo);
         }
+        
+        if(prazo != -1){}
     });
+    console.log(pr)
     if(pr.length != 0){
         sendMessage({"processos": pr});    
     }
+    */
 }
 
 function creteTable(TABLE_GERADOS){
     const table = document.createElement("table")
     const form = document.getElementById("frmProcedimentoControlar");
-    const check = document.querySelector(".table #lnkInfraCheck");
-    const headRow = document.querySelector("#tblProcessosGerados thead").cloneNode(true);
-    headRow.children[0].children[2].children[0].textContent = "Sujeitos a Prazo"
-    document.body.addEventListener("click", (e) => {
-        console.log("a");
-    })
-    console.log(headRow.children[0].children[0].children[0].children[1].setAttribute("onClick", "teste()"));
+   const check = document.createElement("a");
+   const imageCheck = document.createElement("img")
+   const tableHead = document.createElement("thead");
+   const headCells = [];
 
-    /*
-    const head = document.createElement("thead");
-    const body = document.createElement("tbody");
-    const tableTitle = document.createElement("th");
-    const tableDate = document.createElement("th");
-    
-    tableTitle.textContent = "Sujeito a prazo";
-    headRow.appendChild(tableTitle);
-    headRow.appendChild(tableDate);
-
-    */
-    table.appendChild(headRow);
-    
+    for(var i = 0; i < 5; i++){
+        headCells.push(document.createElement("th"));
+        headCells[i].classList.add("tituloControle");
+        headCells[i].style.width = "5%"
+        tableHead.appendChild(headCells[i]);
+    }
+    imageCheck.src = "/infra_css/imagens/check.gif";
+    check.appendChild(imageCheck);
+    headCells[0].appendChild(check);
+    headCells[2].textContent = "Sujeitos a Prazo";
+    headCells[4].textContent = "Prazo";
+    table.id = "tblProcessosComPrazos";
+    table.appendChild(tableHead);
+   
    for(let i = 0; i < TABLE_GERADOS[0].attributes.length; i++){
-    let atributoRenomeado = TABLE_GERADOS[0].attributes[i].value.replace("Gerados", "ComPrazos")
-       table.setAttribute(TABLE_GERADOS[0].attributes[i].name, atributoRenomeado);
+       let atributoRenomeado = TABLE_GERADOS[0].attributes[i].value.replace("Gerados", "ComPrazos")
+       //table.setAttribute(TABLE_GERADOS[0].attributes[i].name, atributoRenomeado);
    }
-   //table.setAttribute("id", "tblProcessosComPrazos");
+
    const container = document.createElement("div");
    const divTabela = document.createElement("div");
    divTabela.classList.add("divPrazosAreaTabela");
-   /*
-   TABLE_GERADOS[0].classList.forEach((element) => {
-       table.classList.add(element);
-   })
-   */
-   container.classList.add("divPrazos")
-   //table.appendChild(headRow);
 
+   container.classList.add("divPrazos")
    table.classList.add("table");
    divTabela.classList.add("infraAreaTabela");
    divTabela.appendChild(table);
    container.appendChild(divTabela);
    form.insertBefore(container, form.childNodes[8]);
    divTabela.setAttribute("id", "teste");
+}
 
+function getData(processo){
+    /*
+    let mouseOverText = "";
+    let data = "";
+    let aux;
+    let prazo;
+    
+    if(processo.children[1].children.length > 0){
+        mouseOverText = processo.children[1].children[0].getAttribute("onmouseover");
+    }
+
+    console.log(processo.children[1].children.length)
+    if(mouseOverText.toLowerCase().lastIndexOf("prazo") != -1 || mouseOverText != "undefined") {
+        let aux = mouseOverText.substring(mouseOverText.lastIndexOf("Prazo"));
+        aux = aux.replace("'", "").replace("Prazo:", "").trim().split(",")[0].split("/").map((element)=>{
+            if(!isNaN(element))
+            return element;
+        });
+        console.log(aux)
+        data = new Date(aux[2], aux[1], aux[0]);
+    }
+    if(data != "Invalid Date"){
+        let now = new Date()
+        console.log(now)
+        prazo = Math.round(Math.abs(data - now) / (1000 * 3600 * 24));
+    }
+    if(mouseOverText != "undefined"){
+        aux = mouseOverText.split("\\n")
+        console.log(aux);
+    }
+    */
+
+    let mouseOverText = processo.getAttribute("onmouseover")
+    let prazo;
+    if(mouseOverText.toLowerCase().lastIndexOf("prazo") != -1 || mouseOverText != "undefined") {
+        let aux = mouseOverText.substring(mouseOverText.lastIndexOf("Prazo"));
+        aux = aux.replace("'", "").replace("Prazo:", "").trim().split(",")[0].split("/").map((element)=>{
+            if(!isNaN(element))
+            return element;
+        });
+        let now = new Date()
+        data = new Date(aux[2], aux[1], aux[0], now.getHours(), now.getMinutes(), now.getSeconds(), now.getMilliseconds());
+        console.log(data)
+        if(data != "Invalid Date"){
+            prazo = Math.abs(data - now) / (1000 * 3600 * 24);
+        }
+    }
+    return (prazo == "undefined") ? "" : prazo;
 }
 
 function sendMessage(message){
