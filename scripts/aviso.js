859b95f5df71f8dff10c4ccc7ddd81e844d9e0fd
Pedro Valladares <66287885+PedroFSValladares@users.noreply.github.com>
2022-08-03 17:26:36 -0300

    Add files via upload
    aplicada alterações de comportamento

--- scripts/aviso.js
+++ scripts/aviso.js
@@ -1,5 +1,4 @@
 //window.addEventListener("loaded", init());
-
 if(document.title === "SEI - Controle de Processos"){
     setTimeout(() => {
     }, 500)
@@ -8,12 +7,11 @@ setTimeout(() => {
     const TABLE_GERADOS = document.querySelectorAll("#tblProcessosGerados");
     const PROCESSOS_GERADOS = getProcessos("Gerados");
     const PROCESSOS_RECEBIDOS = getProcessos("Recebidos");
-    const SERVIDOR_LOGADO = getServidor();
-
+    
     creteTable(TABLE_GERADOS);
     
     //verificaPrazos(PROCESSOS_GERADOS, SERVIDOR_LOGADO);
-    verificaPrazos(SERVIDOR_LOGADO);
+    verificaPrazos();
     //verificaPrazos(PROCESSOS_RECEBIDOS, SERVIDOR_LOGADO); 
 },200)
 function getProcessos(type){
@@ -22,29 +20,40 @@ function getProcessos(type){
         return index >= 1;
     });
 }
-function getServidor(){
-    const servidorLogadoInput = document.getElementById("hdnInfraPrefixoCookie");
-    return servidorLogadoInput.value.substring(servidorLogadoInput.value.lastIndexOf("_") + 1);
-}
-function verificaPrazos(servidor){
+
+function verificaPrazos(){
     let processos = document.querySelectorAll("#divGeradosAreaTabela tbody tr")
     let table = document.getElementById("tblProcessosComPrazos");
-    processos.forEach((element) => {
+    processos.forEach((element,index) => {
         if(element.hasAttribute("id")){
             anotacaoImg = element.querySelector("img[src='imagens/sei_anotacao_pequeno.gif']");
             if(anotacaoImg != null){
-                let prazo = document. createElement("td");
-                prazo.align = "center";
+                let prazoTd = document.createElement("td");
                 let anotacao = anotacaoImg.parentNode;
-                prazo.textContent = getData(anotacao);
-                if(getData(anotacao) > 0 && getData(anotacao) !== ""){
-                    element.appendChild(prazo);
-                    table.appendChild(anotacao.parentNode.parentNode);
-                    chrome.runtime.sendMessage({"processo": "adicionado"}).then(success, error);    
+                let prazo = getData(anotacao);
+                prazoTd.align = "center";
+                prazoTd.textContent = prazo;
+                if(prazo > 0 && prazo !== ""){
+                    let newElement = copyProcess(element, index);
+                    newElement.appendChild(prazoTd);
+                    table.appendChild(newElement);
+                    //chrome.runtime.sendMessage({"processo": "adicionado"}).then(success, error);    
                 }
             }
         }
     })
+}
+
+function copyProcess(processo, i){
+    let pr = document.createElement("tr");
+    let processCells = Array.from(processo.children);
+    console.log(processCells)
+    processCells.forEach((element) => {
+        pr.appendChild(element.cloneNode(true));
+    })
+    pr.children[0].querySelector("input").id = "chkComPrazoItem" + i;
+    pr.classList.add("infraTrClara")
+    return pr;
 }
 
 function creteTable(TABLE_GERADOS){
@@ -61,6 +70,16 @@ function creteTable(TABLE_GERADOS){
         headCells[i].style.width = "5%"
         tableHead.appendChild(headCells[i]);
     }
+
+    check.addEventListener("click", (e) => {
+        let processos = Array.from(document.querySelectorAll("#tblProcessosComPrazos tr"));
+        processos.forEach((element) => {
+            let checkStatus = element.querySelector("input")
+            element.classList.toggle("infraTrMarcada");
+            checkStatus.checked = !(checkStatus.checked);
+        });
+    })
+
     imageCheck.src = "/infra_css/imagens/check.gif";
     check.appendChild(imageCheck);
     headCells[0].appendChild(check);
@@ -69,67 +88,42 @@ function creteTable(TABLE_GERADOS){
     table.id = "tblProcessosComPrazos";
     table.appendChild(tableHead);
 
-for(let i = 0; i < TABLE_GERADOS[0].attributes.length; i++){
-    let atributoRenomeado = TABLE_GERADOS[0].attributes[i].value.replace("Gerados", "ComPrazos")
-    //table.setAttribute(TABLE_GERADOS[0].attributes[i].name, atributoRenomeado);
-}
+    for(let i = 0; i < TABLE_GERADOS[0].attributes.length; i++){
+        let atributoRenomeado = TABLE_GERADOS[0].attributes[i].value.replace("Gerados", "ComPrazos")
+        table.setAttribute(TABLE_GERADOS[0].attributes[i].name, atributoRenomeado);
+    }
 
-const container = document.createElement("div");
-const divTabela = document.createElement("div");
-divTabela.classList.add("divPrazosAreaTabela");
+    const container = document.createElement("div");
+    const divTabela = document.createElement("div");
+    divTabela.classList.add("divPrazosAreaTabela");
 
-container.classList.add("divPrazos")
-table.classList.add("table");
-divTabela.classList.add("infraAreaTabela");
-divTabela.appendChild(table);
-container.appendChild(divTabela);
-form.insertBefore(container, form.childNodes[8]);
-divTabela.setAttribute("id", "teste");
+    container.classList.add("divPrazos")
+    table.classList.add("table");
+    divTabela.classList.add("infraAreaTabela");
+    divTabela.appendChild(table);
+    container.appendChild(divTabela);
+    form.insertBefore(container, form.childNodes[8]);
+    divTabela.setAttribute("id", "teste");
 }
 
 function getData(processo){
-    /*
-    let mouseOverText = "";
-    let data = "";
-    let aux;
+    let mouseOverText = processo.getAttribute("onmouseover")
+    let mouseOverTextFormated = mouseOverText.toLowerCase()
+    console.log(mouseOverTextFormated.lastIndexOf("prazo"))
     let prazo;
-    
-    if(processo.children[1].children.length > 0){
-        mouseOverText = processo.children[1].children[0].getAttribute("onmouseover");
-    }
-
-    console.log(processo.children[1].children.length)
-    if(mouseOverText.toLowerCase().lastIndexOf("prazo") != -1 || mouseOverText != "undefined") {
-        let aux = mouseOverText.substring(mouseOverText.lastIndexOf("Prazo"));
-        aux = aux.replace("'", "").replace("Prazo:", "").trim().split(",")[0].split("/").map((element)=>{
+    if(mouseOverTextFormated.lastIndexOf("prazo") != -1 && mouseOverText != "undefined") {
+        let aux = mouseOverTextFormated.substring(mouseOverTextFormated.lastIndexOf("prazo"));
+        aux = aux.replace("/['-\\/_\",.]/", "").replace("prazo:", "").trim().split(",")[0].split("/").map((element)=>{
             if(!isNaN(element))
             return element;
         });
         console.log(aux)
-        data = new Date(aux[2], aux[1], aux[0]);
-    }
-    if(data != "Invalid Date"){
         let now = new Date()
-        console.log(now)
-        prazo = Math.round(Math.abs(data - now) / (1000 * 3600 * 24));
-    }
-    if(mouseOverText != "undefined"){
-        aux = mouseOverText.split("\\n")
-        console.log(aux);
-    }
-    */
-
-    let mouseOverText = processo.getAttribute("onmouseover")
-    console.log(mouseOverText)
-    let prazo;
-    if(mouseOverText.toLowerCase().lastIndexOf("prazo") != -1 || mouseOverText != "undefined") {
-        let aux = mouseOverText.substring(mouseOverText.lastIndexOf("Prazo"));
-        aux = aux.replace("'", "").replace("Prazo:", "").trim().split(",")[0].split("/").map((element)=>{
-            if(!isNaN(element))
-            return element;
-        });
-        let now = new Date()
-        data = new Date(aux[2], aux[1], aux[0], now.getHours(), now.getMinutes(), now.getSeconds(), now.getMilliseconds());
+        if(aux.length == 2 || aux[2].length == 2){
+            data = new Date(now.getFullYear(), aux[1], aux[0], now.getHours(), now.getMinutes(), now.getSeconds(), now.getMilliseconds());
+        }else{
+            data = new Date(aux[2], aux[1], aux[0], now.getHours(), now.getMinutes(), now.getSeconds(), now.getMilliseconds());
+        }
         console.log(data)
         if(data != "Invalid Date"){
             prazo = Math.abs(data - now) / (1000 * 3600 * 24);
