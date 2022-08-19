054a9701f021829bda00e1458628c1373df5531a
Pedro Henrique Ferreira S. Valladares <pedro.valladares@economia.df.gov.br>
2022-08-19 17:23:42 -0300

    adicionado botão de gestão de prazos

--- controlePrazo.js
+++ controlePrazo.js
@@ -1,11 +1,30 @@
 if(document.title === "SEI - Processo"){
-    init();
+    setTimeout(() => {
+        init();
+    },1500);
 }
 
 function init(){
-    var button = document.createElement("button");
-    var div = document.querySelector("#divInfraAreaTelaD #divArvoreAcoes");
+    var iFrameBody = document.getElementById("ifrVisualizacao").contentWindow.document;
+    var divAcoes = iFrameBody.getElementById("divArvoreAcoes");
+    var btn = createButton(iFrameBody);
+    console.log(iFrameBody)
+    console.log(divAcoes)
+    divAcoes.appendChild(btn);
+}
+function createButton(body){
+    var button = body.createElement("a");
+    var buttonImg = body.createElement("img");
+    var imageURL = chrome.runtime.getURL("./images/prazo-icone.png")
+
+    buttonImg.src = imageURL;
+    buttonImg.classList.add("infraCorBarraSistema")
+    button.appendChild(buttonImg);
     button.classList.add("botaoSEI");
-    console.log(div);
-    div.appendChild(button);
+    button.href = "#";
+
+    return button;
+}
+function createPopup(body){
+    var popup = body.createElement("div");
 }
